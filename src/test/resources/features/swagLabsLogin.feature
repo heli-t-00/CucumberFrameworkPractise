47a127cc217696191ba225d68697ef59783d56b6
Helen To <helen.to@Mac.mynet>
2025-06-24 17:43:10 +0100

    scenario outline worked

--- src/test/resources/features/swagLabsLogin.feature
+++ src/test/resources/features/swagLabsLogin.feature
@@ -12,3 +12,15 @@ Feature: Swag Labs Login
     Given the user enters invalid credentials
     When the user clicks the Login button
     Then the user unsuccessfully login
+
+#    Using Scenario Outline combines both the scenarios above therefore can use Scenario Outline to cover BOTH the scenario rather than having Multiple Step defs
+
+    Scenario Outline: Login attempt with various credentials
+      Given the user enters username "<username>" and password "<password>"
+      When the user clicks the Login button
+      Then the login should be "<result>"
+
+      Examples:
+        | username      | password     | result       |  |
+        | standard_user | secret_sauce | successful   |  |
+        | invalid_user  | wrong_p      | unsuccessful |  |
--- src/test/scala/stepdefs/swagLabsStepDefs.scala
+++ src/test/scala/stepdefs/swagLabsStepDefs.scala
@@ -1,7 +1,7 @@
 package stepdefs
 
 import io.cucumber.scala.{EN, ScalaDsl}
-import locators.loginLocatorsSwagLabs.errorMessage
+import locators.loginLocatorsSwagLabs.{errorMessage}
 import pages.getText.productsOverviewPageSwagLabs.verifyPage
 import pages.loginPageSwagLabs.{buttonLogin, errorMess, inputPassword, inputUser, launchBrowser}
 import testdata.dataSwagLabs.{passWordText, userNameText, userNameWrong}
@@ -26,6 +26,12 @@ class swagLabsStepDefs extends ScalaDsl with EN {
     inputPassword(passWordText)
   }
 
+  Given("""the user enters username {string} and password {string}""") {(username: String, password: String) =>
+inputUser(username)
+    inputPassword(password)
+  }
+
+
   When("""^the user clicks the Login button$""") { () =>
     buttonLogin()
   }
@@ -41,7 +47,14 @@ class swagLabsStepDefs extends ScalaDsl with EN {
     errorMess(errorMessage)
   }
 
+Then("""the login should be {string}""") {(result:String) =>
+if(result == "successful") {
+  verifyPage()
+} else if (result == "unsuccessful") {
+  errorMess(errorMessage)
+}
 
+}
 
 
 
